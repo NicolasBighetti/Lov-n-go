5f904eeaaca706ef71453c1706773c77ffa92f41
Marhold Mônier <marhold.monier@gmail.com>
2018-02-17 19:06:59 +0100

    add stuff and bugs lol

--- LovNgo/www/js/index.js
+++ LovNgo/www/js/index.js
@@ -16,31 +16,81 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+const app = {
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // Application Constructor
+  initialize: function () {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function () {
+    this.receivedEvent('deviceready');/*
+    document.getElementById('photo').addEventListener('click', this.deviceFound, false);
+    alert(`Welcome on Lov'n'Go ! Start looking for people ?`);
+    ble.isEnabled(
+      () => { },
+      () => { ble.enable(() => { console.log('SUCCESS: ble activate by user'); } , () => { console.log('FAILURE: ble activate by user'); }) }
+    );
+    ble.scan([], 60, (device) => {
+      alert(`DEVICE FOUND: ${device}`);
+      this.deviceFound(device);
+    }, () => { console.log('FAILURE: ble scan'); });
+    */
+  },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+    const parentElement = document.getElementById(id);
+    const listeningElement = parentElement.querySelector('.listening');
+    const receivedElement = parentElement.querySelector('.received');
 
-        console.log('Received Event: ' + id);
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  },
+
+  /*
+  deviceFound: function(device) {
+    navigator.vibrate([500, 1000, 500, 1000, 500]);
+    if (confirm('A lovely user found near you, take a picture with him/her ;) ?')) {
+      navigator.camera.getPicture(
+        (picture) => { 
+          console.log('SUCCESS: take pic');
+          this.sendLoveToServer(this.getPosition, picture);
+        },
+        () => { console.log('FAILURE: take pic') });    
     }
+  },
+
+  togglePhoto: function (enable) {
+    if (enable) {
+      document.getElementById('disable').style.backgroundColor = 'rgba(0, 0, 0, 0)';    
+    } else {
+      document.getElementById('disable').style.backgroundColor = 'rgba(0, 0, 0, 0.25)';  
+    }
+  },
+
+  getPosition: function() {
+    let userPosition;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        userPosition = { lat: position.coords.latitude, lng: position.coords.longitude };
+      }
+    );
+    return userPosition;
+  },
+
+  sendLoveToServer: function(position, picture) {
+    alert('LOVE SENT TO SERVER');
+    // TODO
+  }
+  */
+
 };
 
 app.initialize();
